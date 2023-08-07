901ba0d996bfbb1726d3184653583fcd6acc5ab9
yuri-caoution <dldbfl0719@gmail.com>
2023-08-08 08:49:33 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.header').load('../inc/header.html')
+    $('.footer').load('../inc/footer.html')
+
+    // resize, reload
+    // var resizeTimer;
+    // $(window).bind('resize', function( ) {
+    //     window.clearTimeout( resizeTimer );
+    //     resizeTimer = window.setTimeout( resizeFunction, 500 );
+    // });
+    // function resizeFunction(){
+    //     location.reload()
+    // }
+
+
+
+
+})
