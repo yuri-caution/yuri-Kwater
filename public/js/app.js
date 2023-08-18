9192fcafb49d08d611ab43906e7458ffd8eb0963
yuri-caoution <dldbfl0719@gmail.com>
2023-08-18 12:54:10 +0900

    mobile

--- public/js/app.js
+++ public/js/app.js
@@ -12,41 +12,56 @@ $(function(){
     //     location.reload()
     // }
 
-
-    $(window).resize(function(){ 
-        if (window.innerWidth < 1021) {
-            
-        }else{
-            // all
-            $(".fullpage").fullpage({
-                //options here
-                autoScrolling: true,
-                scrollHorizontally: true,
-                menu: ".nav",
-                anchors: ["page1", "page2", "page3", "page4", "page5", "page6"],
-                navigation: false,
-                onLeave: function (anchorLink, index) {
-                    if (index == 3 || index == 6){
-                        $('.nav li a').css('background-color', "#ffffff")
-                        $('.nav li a').find('span').css('color', "#ffffff")
-                    }else{
-                        $('.nav li a').css('background-color', "#333333")
-                        $('.nav li a').find('span').css('color', "#333333")
-                    }
-                },
-            });
-
-        }
-    })
+    // all
+    $(".fullpage").fullpage({
+        //options here
+        autoScrolling: true,
+        scrollHorizontally: true,
+        menu: ".nav",
+        anchors: ["page1", "page2", "page3", "page4", "page5", "page6"],
+        navigation: false,
+        onLeave: function (anchorLink, index) {
+            if (index == 3 || index == 6){
+                $('.nav li a').css('background-color', "#ffffff")
+                $('.nav li a').find('span').css('color', "#ffffff")
+            }else{
+                $('.nav li a').css('background-color', "#333333")
+                $('.nav li a').find('span').css('color', "#333333")
+            }
+        },
+        responsiveWidth: 1021
+    });
 
     // intro
+    $('.nav_bar').on('init reInit', function (event, slick) {
+        //페이징이니셜
+        $("#nav_paging").html('<span class="page">'+ (slick.currentSlide+1) +'</span> / ' + 3);
+    }).on('beforeChange', function(event, slick, currentSlide, nextSlide){ 
+        //슬라이드 변경 시 페이징 변경
+        var i = (nextSlide ? nextSlide : 0) + 1;
+        $("#nav_paging").find(".page").text(i);
+    })
+
+
     $('.nav_bar').slick({
         slidesToShow: 8,
         slidesToScroll: 1,
         prevArrow : $('.prevArrow'), 
         nextArrow : $('.nextArrow'),
-        infinite:false
+        infinite:false,
+        responsive: [ // 반응형 웹 구현 옵션
+        {  
+            breakpoint: 1020, 
+            settings: {
+                rows: 2,
+                slidesPerRow: 3,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+          } 
+        }
+        ]
     })
+
 
     $('.prevArrow, .nextArrow').hover(function(){
         $(this).find('img').attr('src', $(this).find('img').attr('src').replace(".svg", "_fff.svg"))
